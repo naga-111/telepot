00672e9738610990a706a3a8d099242f77e18498
naga-111 <ayongeorge@gmail.com>
2022-10-22 12:24:51 +0800

    first-commit

---
+++ pot.py
@@ -0,0 +1,6 @@
+import telepot
+
+bot = telepot.Bot("5430181086:AAHI3fHzWGTHCBxbsb32qWnqFr5NkjQyv5U")
+
+bot.sendPhoto(chat_id="744591652", photo=("https://media.moddb.com/images/members/5/4550/4549205/dog.jpg"))
+
